299ad10ce43bcc50f8ddd7754fa12f9fb30e22ca
stickman221 <stickman221@1773bc93-3b4e-b3bc-e02a-bb6a76b80a96>
2011-04-19 22:31:34 +0000

    dk64 code generator program files

---
+++ trunk/N64 Programs/DK64 Title Generator/stdafx.cpp
@@ -0,0 +1,10 @@
+// stdafx.cpp : source file that includes just the standard includes
+// DK64codegen.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "StdAfx.h"
+#include <fstream>
+#include <string>
+#include <iostream>
+
+
